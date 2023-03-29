b70c40b91f495e0df48f94ef9ffd74d9d04dd1e1
YDH <swimmingpolar@gmail.com>
2023-03-29 13:05:44 +0900

    chore: chromatic build test

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
-import path from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    dts({
-      insertTypesEntry: true
-    })
-  ],
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -22,22 +14,5 @@ export default defineConfig({
     reporters: 'verbose',
     include: ['**/*.test.tsx'],
     cache: false
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'index',
-      formats: ['es', 'umd'],
-      fileName: format => `index.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
   }
 })
